40148a6b6b3a7d0b318ef813c69846ab828b41eb
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-10-24 15:35:21 +0300

    This commit adds functionality tosave, delete,find and display credentials

--- run.py
+++ run.py
@@ -29,3 +29,37 @@ def create_credentials(platform,username,email,password):
     """
     new_credentials = Credentials(platform,username,email,password)
     return new_credentials
+
+def save_credentials(credentials):
+    """
+    Function to save credentials
+    """
+    credentials.save_credentials()
+
+
+def delete_credential(credentials):
+    """
+    Function to delete credentials
+    """
+    credentials.delete_credentials()
+
+
+def find_credentials(platform):
+    """
+    Function that finds a credential by platform name and returns the credentials
+    """
+    return Credentials.find_by_platform(platform)
+
+
+def check_existing_credentials(platform):
+    """
+    Function that check if a credential exists with that number and return a Boolean
+    """
+    return Credentials.credentials_exists(platform)
+
+
+def display_credentials():
+    """
+    Function that returns all the saved credentials
+    """
+    return Credentials.display_credentials()
