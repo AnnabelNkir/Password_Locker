2046989c695aea954539aa3414b245be02355a19
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-10-24 12:40:58 +0300

    This commit tests initialization of the object

--- credentials_test.py
+++ credentials_test.py
@@ -15,3 +15,13 @@ class TestCredentials(unittest.TestCase):
         Set up method to run before each test cases.
         """
         self.new_credentials = Credentials("Github", "user101", "user101@email", "QwerY23")
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_credentials.platform, "Github")
+        self.assertEqual(self.new_credentials.username, "user101")
+        self.assertEqual(self.new_credentials.email, "user101@email")
+        self.assertEqual(self.new_credentials.password, "QwerY23")
+    
